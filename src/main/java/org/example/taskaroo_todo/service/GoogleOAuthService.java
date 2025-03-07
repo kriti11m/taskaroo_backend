92030135bcb0145adb9fa3c6773d51b50cee99d7
Raghav Agrawal <raghav.github69@gmail.com>
2025-03-08 01:23:31 +0530

    google auth changes

--- src/main/java/org/example/taskaroo_todo/service/GoogleOAuthService.java
+++ src/main/java/org/example/taskaroo_todo/service/GoogleOAuthService.java
@@ -6,9 +6,7 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import org.example.taskaroo_todo.model.User;
 import org.example.taskaroo_todo.repository.UserRepo;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -21,8 +19,6 @@ public class GoogleOAuthService {
     @Autowired
     private UserRepo userRepository;
 
-
-
     public GoogleOAuthService() {
         this.verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
                 .setAudience(Collections.singletonList("484818255394-7fttjna8nsgk0bs3iflncoi95ruestja.apps.googleusercontent.com"))
@@ -34,17 +30,21 @@ public class GoogleOAuthService {
         if (idToken != null) {
             GoogleIdToken.Payload payload = idToken.getPayload();
             String email = payload.getEmail();
-            String name = (String) payload.get("name");
-             // Set a default password or generate one
 
-            User user = new User();
-            user.setEmail(email);
-            user.setName(name);
-
-
-            userRepository.save(user);
-
-            return payload;
+            // Check if user exists
+            User existingUser = userRepository.findByEmail(email);
+            if (existingUser != null) {
+                // User exists, return payload
+                return payload;
+            } else {
+                // User does not exist, create new user
+                String name = (String) payload.get("name");
+                User newUser = new User();
+                newUser.setEmail(email);
+                newUser.setName(name);
+                userRepository.save(newUser);
+                return payload;
+            }
         } else {
             throw new Exception("Invalid ID token.");
         }
